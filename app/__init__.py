d8802d52111003bb01088b3dcd9cb459f792c62a
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2015-12-04 22:38:20 +0000

    Clean up __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, g
 from flask.ext.login import LoginManager, current_user
+from peewee import DoesNotExist
 from .models import DATABASE
 from .auth.models import User
 
@@ -14,20 +15,21 @@ login_manager.login_view = 'login'
 # Login Manager Functions
 
 @login_manager.user_loader
-def load_user(userid):
+def load_user(user_id):
     try:
-        return User.get(User.id == userid)
-    except:
+        return User.get(User.id == user_id)
+    except DoesNotExist:
         return None
 
-# Before and After request functions
 
+# Before and After request functions
 @app.before_request
 def before_request():
     """Connect to database before each request"""
     g.db = DATABASE
     g.db.connect()
     g.user = current_user
+
 
 @app.after_request
 def after_request(response):
