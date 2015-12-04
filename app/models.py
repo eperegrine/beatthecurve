92e6f3db5ab7c5856294d5d7184c47f079c80f2a
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2015-12-04 08:18:08 +0000

    Add DATABASE

---
+++ app/models.py
@@ -0,0 +1,28 @@
+from peewee import *
+import os
+import urllib.parse
+
+if 'LOCAL_DEV' in os.environ:
+    database = {
+        'engine': 'peewee.PostgresqlDatabase',
+        'name': 'BEATTHECURVE',
+        'user': 'charliethomas',
+        'password': '',
+        'host': '127.0.0.1',
+        'port': '5432',
+    }
+else:
+    urllib.parse.uses_netloc.append('postgres')
+    url = urllib.parse.urlparse(os.environ['DATABASE_URL'])
+
+    database = {
+        'engine': 'peewee.PostgresqlDatabase',
+        'name': url.path[1:],
+        'user': url.username,
+        'password': url.password,
+        'host': url.hostname,
+        'port': url.port,
+    }
+
+DATABASE = PostgresqlDatabase(database['name'], user=database['user'], password=database['password'],
+                              host=database['host'], port=database['port'])
