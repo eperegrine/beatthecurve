b4c96db4b05ed797c92b67a00779445caa82d983
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2015-12-13 18:51:14 +0000

    Add AddDiscussionForm

--- app/notes/forms.py
+++ app/notes/forms.py
@@ -10,7 +10,7 @@ from wtforms.validators import (
     Optional
 )
 
-from .models import Lecture
+from .models import Lecture, Discussion
 
 
 def lecture_name_in_use_validator(form, field):
@@ -20,7 +20,15 @@ def lecture_name_in_use_validator(form, field):
         raise ValidationError('Name already in use')
 
 
+def discussion_name_in_use_validator(form, field):
+    if Discussion.select().where(
+                    (Discussion.name == form.data) & (Discussion.lecture_id == int(form.lecture.data))
+    ).exists():
+        raise ValidationError('Name already in use')
+
+
 class AddLectureForm(Form):
+    # TODO: Add validators
     lesson = SelectField('Lesson')
     name = StringField('Name')
 
@@ -29,3 +37,8 @@ class AddNoteForm(Form):
     lecture = SelectField('Lecture', validators=[DataRequired()])
     discussion = SelectField('Discussion', validators=[Optional()])
     file = FileField('File', validators=[DataRequired()])
+
+
+class AddDiscussionForm(Form):
+    lecture = SelectField('Lecture', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired(), discussion_name_in_use_validator])
