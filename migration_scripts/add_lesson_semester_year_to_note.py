edb3b755ec7b7817b4b1ed91ee75b6191d495530
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2016-01-28 07:14:04 +0000

    Update migration

--- migration_scripts/add_lesson_semester_year_to_note.py
+++ migration_scripts/add_lesson_semester_year_to_note.py
@@ -8,8 +8,25 @@ Note.lesson.default = 2
 Note.semester.default = 1
 Note.year.default = 2016
 
-migrate(
-    migrator.add_column('TBL_NOTE', Note.lesson.db_column, Note.lesson),
-    migrator.add_column('TBL_NOTE', Note.semester.db_column, Note.semester),
-    migrator.add_column('TBL_NOTE', Note.year.db_column, Note.year),
-)
+try:
+    migrate(
+        migrator.add_column('TBL_NOTE', Note.semester.db_column, Note.semester)
+    )
+except:
+    pass
+
+try:
+    migrate(
+        migrator.add_column('TBL_NOTE', Note.year.db_column, Note.year)
+    )
+except:
+    pass
+
+
+try:
+    migrate(
+        migrator.add_column('TBL_NOTE', Note.lesson.db_column, Note.lesson)
+    )
+except:
+    pass
+
