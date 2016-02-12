2d144bb6e35e3a19a886fe6cb23ebc6449678870
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2016-02-12 13:33:07 +0000

    Add docstrings to auth/forms.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -14,21 +14,24 @@ from .models import User, School
 
 
 def edu_email_validator(form, field):
-    print(field.data[-4:])
+    """Raises a ValidationError if an email does not have the .edu top level domain"""
     if not field.data[-4:] == '.edu':
         raise ValidationError('Email must be a .edu email')
 
 
 def email_in_use_validator(form, field):
+    """Raises a ValidationError if an email is already being used"""
     if User.select().where(User.email == form.data).exists():
         raise ValidationError('Email already in use')
 
-def get_school():
+
+def get_schools():
+    """Returns a list of tuples containing all schools' ids and names"""
     return [(str(school.school_id), school.name) for school in School.select()]
 
 
-
 class SignUpForm(Form):
+    """Form to allow users to sign up"""
     first_name = StringField('First Name', validators=[DataRequired()])
     last_name = StringField('Last Name', validators=[DataRequired()])
     email = StringField('.edu Email',
@@ -36,22 +39,25 @@ class SignUpForm(Form):
                                     Email(),
                                     edu_email_validator,
                                     email_in_use_validator])
-    school = SelectField('School', choices=[("-1", "Choose a university")] + get_school())
+    school = SelectField('School', choices=[("-1", "Choose a university")] + get_schools())
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password',
                                                                                       message='Passwords must match')])
 
 
 class LoginForm(Form):
+    """Form to allow users to login using their email and password"""
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
 
 
 class ChangeEmailForm(Form):
+    """Form to allow users to update their email"""
     email = StringField('Email', validators=[DataRequired(),Email(), edu_email_validator, email_in_use_validator])
 
 
 class ChangePasswordForm(Form):
+    """Form to allow users to update their password. It requires them to have access to their origin password."""
     current_password = PasswordField('Old Password', validators=[DataRequired()])
     new_password = PasswordField('New Password', validators=[DataRequired()])
     new_password2 = PasswordField('Confirm New Password', validators=[DataRequired(), EqualTo('new_password',
@@ -59,5 +65,6 @@ class ChangePasswordForm(Form):
 
 
 class UpdatePermissions(Form):
+    """Form to grant a user a permission"""
     user = SelectField('Users', validators=[DataRequired()])
     permission = SelectField('Permission', validators=[DataRequired()], choices=[("-1", "Choose a permission")])
