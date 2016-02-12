3b91874ed01346eb8176343217947cbfda20e52c
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2016-02-12 13:51:17 +0000

    Add docstrings to auth/decorators.py

--- app/auth/decorators.py
+++ app/auth/decorators.py
@@ -4,6 +4,11 @@ from .models import Permission, UserPermission
 
 
 def permission_required(name):
+    """Decorator to wrap view functions to restrict their access to users with a given permissions
+
+    :param name: The name of the permission required
+    :type name: str
+    """
     def decorator(view_func):
         @wraps(view_func)
         def wrapper(*args, **kwargs):
@@ -16,6 +21,11 @@ def permission_required(name):
 
 
 def either_permission_required(names):
+    """Decorator to wrap view functions to restrict their access to users which have any one of a list of permissions
+
+    :param names: list of permission names which a user is required to have one of
+    :type names: list
+    """
     def decorator(view_func):
         @wraps(view_func)
         def wrapper(*args, **kwargs):
