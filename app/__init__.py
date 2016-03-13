b3a7c007536f8c991228b35ab0a1435376acb5d1
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2016-03-13 16:44:24 +0000

    Add get_unattended_lessons:

--- app/__init__.py
+++ app/__init__.py
@@ -138,4 +138,4 @@ from app.chat.views import chat_bp
 app.register_blueprint(chat_bp)
 
 from app.lesson.models import LessonStudent
-app.jinja_env.globals.update(get_lessons=LessonStudent.get_attended_lessons)
+app.jinja_env.globals.update(get_lessons=LessonStudent.get_attended_lessons, get_unattended_lessons=Lesson.get_unattended_lessons)
