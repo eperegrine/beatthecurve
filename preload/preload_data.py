7881e4f5d428d95b7ad974fdaa97f876c1ef88d2
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2016-03-29 18:32:31 +0100

    Update preload_data

--- preload/preload_data.py
+++ preload/preload_data.py
@@ -8,21 +8,19 @@ data = json.load(f)
 school = School.get()
 
 for key, value in data.items():
-    if value["link"] is not None and "professors" in value.keys():
-        lesson = Lesson.create(
-            code=key,
-            lesson_name=value["title"],
-            school_id=school.school_id
-        )
-
-        if value["professors"][0] is not None:
-            for p in value["professors"]:
-                if p is not None:
-                    name = p.split(" ")
-                    Professor.create(
-                        lesson_id=lesson.id,
-                        first_name=name[0],
-                        last_name=name[1]
-                    )
+    if value["link"] is not None and "professors" in value.keys() and value["professors"][0] is not None:
+        for p in value["professors"]:
+            lesson = Lesson.create(
+                code=key,
+                lesson_name=value["title"],
+                school_id=school.school_id
+            )
+            if p is not None:
+                name = p.split(" ")
+                Professor.create(
+                    lesson_id=lesson.id,
+                    first_name=name[0],
+                    last_name=name[1]
+                )
 
 
